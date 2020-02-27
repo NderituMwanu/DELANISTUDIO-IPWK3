e508c29e50755bd707ae8cd30adf2b0514332884
NderituMwanu <mwanugerald@gmail.com>
2020-02-27 12:39:00 +0300

    added toggle()

---
+++ js/scriptz.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#whatwedo1").click(function(){
+        $("#imgs1").toggle();
+        $("#text1").toggle();
+    });
+
+            $("#whatwedo2").click(function(){
+                $("#imgs2").toggle();
+                $("#text2").toggle();
+            });
+                $("#whatwedo3").click(function(){
+                    $("#imgs3").toggle();
+                    $("#text3").toggle();
+                })
+
+   
+}); 
