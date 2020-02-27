0013a3a0dbcffeb6f0b4dd4edf82c75221499925
NderituMwanu <mwanugerald@gmail.com>
2020-02-27 14:10:38 +0300

    it will make changes to the javascript code

--- js/scriptz.js
+++ js/scriptz.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
                 $("#whatwedo3").click(function(){
                     $("#imgs3").toggle();
                     $("#text3").toggle();
-                })
+                });
 
    
 }); 
